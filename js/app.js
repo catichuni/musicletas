924766dc4b4cb07c225edd3013db045172afc57f
catichuni <catalina.her93@gmail.com>
2020-08-18 18:07:11 -0300

    f

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const navSlide = () =>{
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.link-items');
+    const contact= document.querySelector('.contacto');
+    const navLinks = document.querySelectorAll('.link-items li');
+    const navBack = document.querySelector('nav');
+    //toggle Nav
+    menu.addEventListener('click', ()=>{
+        nav.classList.toggle('link-items-active');
+
+        //animate links
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.5}s`;
+            }
+        });
+        //menu animate
+        menu.classList.toggle('toggle');
+
+        navBack.classList.toggle('active');
+    });    
+    //animate links
+  
+
+}
+
+navSlide();
+
